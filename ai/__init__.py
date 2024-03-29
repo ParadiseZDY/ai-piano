5b6b6da9aa3a10affe98d790eafdb281ccee5184
zhaodongyangbj <zhaodongyangbj@yuanfudao.com>
2024-03-29 11:27:53 +0800

    初始化web

--- ai/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding:utf-8 -*-
-# Author:ZDY
-# Date: 2024/3/28 3:03 PM
--- ai/web/music.py
+++ ai/web/music.py
@@ -11,5 +11,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+#top k
+
+#分享-生成二维码
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
